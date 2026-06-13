eac40bceebc735a369b949484d007062a9b50a82
Preetam Singh <spreetam2001@gmail.com>
2026-06-13 19:16:18 +0530

    Initial commit

---
+++ src/main/java/com/preesa/razorpay/RazorpayApplication.java
@@ -0,0 +1,15 @@
+package com.preesa.razorpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazorpayApplication {
+
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RazorpayApplication.class, args);
+	}
+
+}
