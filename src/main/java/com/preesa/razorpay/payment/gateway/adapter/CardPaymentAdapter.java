262e423f92bda988037764cbd37b51a9761f85ce
Preetam Singh <spreetam2001@gmail.com>
2026-07-05 19:26:27 +0530

    Add code changes for initiate (adapter -> router) and capture payment.

--- src/main/java/com/preesa/razorpay/payment/gateway/adapter/CardPaymentAdapter.java
+++ src/main/java/com/preesa/razorpay/payment/gateway/adapter/CardPaymentAdapter.java
@@ -3,7 +3,13 @@ package com.preesa.razorpay.payment.gateway.adapter;
 import com.preesa.razorpay.payment.gateway.PaymentAdapter;
 import com.preesa.razorpay.payment.gateway.dto.PaymentRequest;
 import com.preesa.razorpay.payment.gateway.dto.PaymentResult;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
+@Component
+@Slf4j
+@RequiredArgsConstructor
 public class CardPaymentAdapter implements PaymentAdapter {
     /**
      * @param request
